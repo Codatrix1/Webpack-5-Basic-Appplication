5ad9a3b4e78259e6fd9bfaf9a3087708acdcc528
Codatrix <ankur.codatrix@gmail.com>
2022-03-22 20:41:01 +0530

    webpack basic config setup - additional dev dependencies added for sass and css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+//------------------------
+// ⭐ Code for npm test
+//------------------------
+// import { v4 as uuidv4 } from "uuid";
+// console.log(uuidv4());
+
+//-----------------------------
+// ⭐ Code for Our Webpack App
+//-----------------------------
 import generateJoke from "./generateJoke";
 
 console.log(generateJoke());
